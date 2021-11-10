f34cc88c1b5c34fd8bc79bb11caa3c252b282f77
Nicolas Navarro <nicolas.navarro@engee.com.ar>
2021-11-10 13:56:34 -0300

    Agregar archivos de proyecto.

---
+++ challengeZ/Controllers/ExampleController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challengeZ.Controllers
+{
+    [ApiController]
+    [Route("api/ [controller]")]
+    public class ExampleController : ControllerBase
+    {
+
+        private readonly ILogger<ExampleController> _logger;
+
+        public ExampleController(ILogger<ExampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Try with POST method ;)";
+        }
+
+        [HttpPost]
+        public ActionResult<decimal[]> Post(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+                return BadRequest();
+
+            return MasMenos(numbers);
+        }
+
+        private decimal[] MasMenos(int[] numbers) 
+        {
+            decimal totalNumbers = numbers.Length;
+            decimal positivos = numbers.Where(n => n > 0).Count() / totalNumbers;
+            decimal negativos = numbers.Where(n => n < 0).Count() / totalNumbers;
+            decimal ceros = numbers.Where(n => n == 0).Count() / totalNumbers;
+
+            return new decimal[] { decimal.Round(positivos,2) , decimal.Round(negativos, 2), decimal.Round(ceros, 2) };
+        }
+
+    }
+}
